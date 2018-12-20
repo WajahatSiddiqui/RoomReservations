951fa1e073cfdc52fb28a769adce2fcad8596802
WajahatSiddiqui <wajahatmeister@gmail.com>
2018-12-20 20:12:22 +0530

    Adding config, discovery and zuul proxy

---
+++ zuul-proxy/src/main/java/com/wajahat/zuulproxy/ZuulProxyApplication.java
@@ -0,0 +1,14 @@
+package com.wajahat.zuulproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+
+}
+
